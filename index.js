27b9b97717c1a1153f210799b44d161f2cde0db9
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-05-13 07:26:33 +0600

    edit some code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const app = express()
+
 const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
